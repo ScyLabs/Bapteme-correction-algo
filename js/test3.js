4d9392716287bf8e75dfd9d4ed622fcc1c3fc1f9
Philippe Candille <philippe@oclock.io>
2017-06-16 10:57:33 +0200

    first commit

---
+++ js/test3.js
@@ -0,0 +1,29 @@
+/*
+ * Tests
+ */
+var container = document.getElementById('container');
+if (
+  getExtension('test1.css') === 'css'
+  && getExtension('jquery.min.js') === 'js'
+  && getExtension('.eslintrc') === 'eslintrc'
+  && getExtension('monfichier') === false
+) {
+  container.textContent = 'Yeaaah';
+  container.className = 'success';
+}
+else {
+  container.textContent = 'Nope';
+  container.className = 'error';
+}
+
+
+/*
+ * Exo3
+ *
+ * str est une string
+ * Retourner l'extension de fichier.
+ * Par exemple index.html => html
+ */
+function getExtension(str) {
+
+}
